bb13caee795c2cd2be538e50f295303de98dffd6
Jorge Matos <jisapjisap@gmail.com>
2023-08-08 20:17:09 +0200

    Clerk-Auth

---
+++ middleware.ts
@@ -0,0 +1,10 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+    publicRoutes: ['/', '/api/webhook/clerk'], // An array of public routes that don't require authentication.
+    ignoredRoutes: ['/api/webhook/clerk']      // An array of routes to be ignored by the authentication middleware.
+});
+
+export const config = {
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
