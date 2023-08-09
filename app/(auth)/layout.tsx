e2a8099db81eb116c534ac1ca73e4462a8e1b19a
Jorge Matos <jisapjisap@gmail.com>
2023-08-09 18:06:38 +0200

    topbar

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ import '../globals.css';
 
 export const metadata = {
     title: 'Threads',
-    description: 'A Next.js 13 Meta Threads Appliacation'
+    description: 'A Next.js 13 Meta Threads Application'
 }
 
 const inter = Inter({ subsets: ["latin"]});
