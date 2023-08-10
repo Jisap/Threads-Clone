6519e8f35ee8708c2cac1c1c85d33c7497007e8b
Jorge Matos <jisapjisap@gmail.com>
2023-08-10 20:06:48 +0200

    rightSidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -24,7 +24,8 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <Topbar />
-          <main>
+          {/* max-xl:hidden */}
+          <main className='flex flex-row'>
             <LeftSidebar />
             <section className='main-container'>
               <div className='w-full max-w-4xl'>
